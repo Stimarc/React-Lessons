b97fdcda684d7adf3052ac63451329aca518f167
Stimarc <124087165+Stimarc@users.noreply.github.com>
2023-11-14 21:11:15 +0200

    add

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -1,9 +1,36 @@
+import { useState } from 'react';
 import './App.css';
+import { Products } from './components/Products';
+
 
 const App = () => {
+
+   const products = [
+    {id:1,title:"Product 1",price:10890,img:"p1.webp"},
+    {id:1,title:"Product 1",price:15720,img:"p2.webp"},
+    {id:1,title:"Product 1",price:9876,img:"p3.webp"},
+   ];
+
+   const [cart, setCart] = useState([]);
+
+   const addToCart = (id) => {
+    setCart([...cart,id]);
+   }
+
   return (
    <div className='App'>
-    <h1>Shop</h1>
+    <div className="header">
+      <div className="logo">SUPER SHOP</div>
+      <div className="cart">{ cart.length }</div>
+    </div>
+    <div className="main">
+
+      <Products data={products} addToCartHandler={ addToCart } />
+
+    </div>
+
+
+    <div className="footer"></div>
    </div>
   );
 }
