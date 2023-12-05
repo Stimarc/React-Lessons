c4dd20dd6d70bca76e658853d3857a5e5292e29e
Stimarc <124087165+Stimarc@users.noreply.github.com>
2023-12-05 18:13:58 +0200

    del

--- shop/src/utils/getCartProductQty.js
+++
@@ -1,6 +0,0 @@
-export const getCartProductQty = (cartObj) => {
-    const countsArr = Object.values(cartObj);
-    const count = countsArr.reduce((total,item) => total + item.qty , 0);
-    
-    return  count;
- }
--- shop/src/utils/index.js
+++
@@ -1,2 +0,0 @@
-export * from './getCartProductQty';
-export * from './getCartObj';
