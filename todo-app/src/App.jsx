82f804cd0c26589a20ce256fa5f2a55fa68835e6
Stimarc <124087165+Stimarc@users.noreply.github.com>
2023-12-11 21:58:38 +0200

    add use LocalStorage

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,49 +1,79 @@
 import  { useState, useEffect } from 'react';
+import useLocalStorage from './hooks/useLocalStorage';
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useLocalStorage('todos', []);
+  const [newTodo, setNewTodo] = useState('');
+  const [showCompleted, setShowCompleted] = useState(true);
 
   useEffect(() => {
+   
+  }, []); 
+  const handleAddTodo = () => {
+    if (newTodo.trim() === '') return;
 
-     (async () => {
-      const todosRes = await fetch ('http://localhost:3000/todos');
-      const todos = await todosRes.json();
-      setTodos(todos);
+   
+    setTodos([...todos, { id: todos.length + 1, body: newTodo, completed: false }]);
+    setNewTodo('');
+  };
 
-      })()
-
-    
-  }, []);
+  const handleToggleCompleted = (id, completed) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
 
   return (
     <div className="App">
-      <h2>Todo list from API</h2>
+      <h2>Список завдань</h2>
 
       <div className="todo-app">
-        <div className="todo-add-form"></div>
+        <div className="todo-add-form">
+          <input
+            type="text"
+            placeholder="Додати нове завдання"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button onClick={handleAddTodo}>Додати завдання</button>
+        </div>
+
         <div className="todo-actions">
           <label>
-          <input type="checkbox" name="" id="" />
-          no completed
+            <input
+              type="checkbox"
+              checked={showCompleted}
+              onChange={() => setShowCompleted(!showCompleted)}
+            />
+            Показувати тільки виконані
           </label>
-          
         </div>
 
         <div className="todo-list">
-          <h3 className="todo-list__title"></h3>
+          <h3 className="todo-list__title">Список завдань</h3>
 
           <ul className="todo-list__content">
-            {
-               todos.length
-              ? todos.map((todo, index) => <li key={todo.id}> {index + 1} {todo.body} </li>
-                )
-              : <p>Todo list is empty</p>
-            }
+            {todos.length ? (
+              todos
+                .filter((todo) => (showCompleted ? true : !todo.completed))
+                .map((todo) => (
+                  <li
+                    key={todo.id}
+                    onClick={() => handleToggleCompleted(todo.id, todo.completed)}
+                    style={{ cursor: 'pointer', textDecoration: todo.completed ? 'line-through' : 'none' }}
+                  >
+                    {todo.body}
+                  </li>
+                ))
+            ) : (
+              <p>Список завдань порожній</p>
+            )}
           </ul>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
