0b49e5728e495139fa2e1b108081f0009af52192
Stimarc <124087165+Stimarc@users.noreply.github.com>
2023-12-19 17:46:02 +0200

    add

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -1,40 +1,40 @@
-import { Route, Routes } from 'react-router-dom';
-import { 
-  HomePage, 
-  PostsPage, 
-  ContactsPage, 
-  NotFoundPage, 
-  AboutPage, 
-  SinglePostPage 
-} from './pages';
+import { Route, Routes } from 'react-router-dom'; 
 import { Layout } from './components';
-
+import { 
+  AboutPage, 
+  ContactsPage, 
+  HomePage, 
+  NotFoundPage, 
+  PostsPage,
+  SinglePostPage, 
+} from './pages';
 
 const App = () => {
   const posts = [
-    {id:1,title:'Post 1 title',text:'Post 1 Text'},
-    {id:2,title:'Post 2 title',text:'Post 2 Text'},
-    {id:3,title:'Post 3 title',text:'Post 3 Text'},
-    {id:4,title:'Post 4 title',text:'Post 4 Text'},
-  ]
+    { id: 1, title: 'Post 1 title', text: 'Post 1 Text' },
+    { id: 2, title: 'Post 2 title', text: 'Post 2 Text' },
+    { id: 3, title: 'Post 3 title', text: 'Post 3 Text' },
+    { id: 4, title: 'Post 4 title', text: 'Post 4 Text' },
+  ];
 
   return (
-    <div className="container full height">
+    <div className="container full-height">
       <Routes>
 
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={ <Layout /> } >
 
-        <Route index path="/" element={<HomePage />} />
+          <Route index element={ <HomePage /> } />
 
-        <Route path="posts" element={<PostsPage data={posts}/>} />
-        <Route path="posts/:id" element={<SinglePostPage data={posts} />} />
-        <Route path="contacts" element={<ContactsPage />} />
-        <Route path="about" element={<AboutPage />} />
+          <Route path="posts" element={ <PostsPage data={ posts }/> } />
+          <Route path="posts/:id" element={ <SinglePostPage data={ posts }/> } />
+          <Route path="contacts" element={ <ContactsPage /> } />
+          <Route path="about" element={ <AboutPage /> } />
 
-        <Route path="*" element={ <NotFoundPage />} />
+          <Route path="*" element={ <NotFoundPage /> } />
 
+        </Route>
+        
       </Routes>
-
     </div>
   );
 }
