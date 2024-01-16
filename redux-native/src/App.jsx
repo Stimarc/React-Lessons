1f8ada6687f4f1efce8ac9b101b903ce9e68f125
Stimarc <124087165+Stimarc@users.noreply.github.com>
2024-01-16 18:17:01 +0200

    add

--- redux-native/src/App.jsx
+++ redux-native/src/App.jsx
@@ -1,18 +1,30 @@
+import React from "react";
 import { AddUser } from "./components/AddUser";
 import MainInfo from "./components/MainInfo";
 import Users from "./components/Users";
 
+
 const App = () => {
+  const roles = [];
+
   return (
     <div className="container">
-    <h1>Redux Native (useReducer + useContext)</h1>
+      <h1>Redux Native (useReducer + useContext)</h1>
 
-    <div className="users-app">
-       <MainInfo />
+      <div className="users-app">
+        <MainInfo />
         <AddUser />
-         <Users />
-    </div>
+        <Users />
+      </div>
 
+      <div className="roles">
+        <h2>Roles:</h2>
+        <ul>
+          {roles.map((role) => (
+            <li key={role}>{role}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- redux-native/src/components/AddUser.jsx
+++ redux-native/src/components/AddUser.jsx
@@ -1,18 +1,22 @@
+import React, { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+
 export const AddUser = () => {
+  const { dispatch } = useContext(UserContext);
 
-    const clickHandler = () => {
-        const id = Math.random();
-        const newUser = {
-            id,
-            title: 'user-' + id,
-         }
-
-         console.log(newUser);
+  const clickHandler = () => {
+    const id = Math.random();
+    const newUser = {
+      id,
+      title: 'user-' + id,
     }
-    return (
-        <div className="add-user">
-          <button onClick={ clickHandler }>add user</button>  
-        </div>
-    );
-};
 
+    dispatch({ type: 'ADD_USER', user: newUser });
+  }
+
+  return (
+    <div className="add-user">
+      <button onClick={clickHandler}>add user</button>
+    </div>
+  );
+};
