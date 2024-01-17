5c0093dce9e9a2fe6c8f37d5bc98b9991e827533
Stimarc <124087165+Stimarc@users.noreply.github.com>
2024-01-17 19:41:39 +0200

    add

--- redux-toolkit-app/src/index.js
+++ redux-toolkit-app/src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={ store }>
+  <BrowserRouter>
+    <Provider store={ store }>
       <App />
   </Provider>
     
   
+  </BrowserRouter>
+
 );
 
